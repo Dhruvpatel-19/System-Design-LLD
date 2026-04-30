f2bf6575026ee83a89884ec682737dbadb63ac4a
Dhruv Patel <dhruvpatel19201@gmail.com>
2026-04-29 23:39:17 -0400

    Add Adapter, Bridge and Composite patterns and fix some classes names

---
+++ src/structuralpatterns/StructuralPatterns.java
@@ -0,0 +1,51 @@
+package structuralpatterns;
+
+/*
+ * Sturctural Design Patterns (Java)
+ *
+ * Sturctural patterns deal with deal with object composition.
+ * They help with how classes and objects are combined to form larger, flexible, and efficient structures.
+ * 
+ * ================================
+ * 1. Adapter Pattern
+ * ================================
+ * Converts the interface of one class into another interface expected by the client.
+ * Allows incompatible interfaces to work together without modifying existing code.
+ * Useful when integrating legacy systems or third-party libraries.
+ * 
+ *
+ *
+ * ================================
+ * 2. Bridge Pattern
+ * ================================
+ * Decouples abstraction from implementation so that both can vary independently.
+ * Instead of creating many class combinations, it splits hierarchy into:
+ * - Abstraction (high-level control)
+ * - Implementation (low-level details)
+ * Useful when both abstractions and implementations are expected to change frequently.
+ *
+ * 
+ *
+ *
+ * ================================
+ * 3. Composite Pattern
+ * ================================
+ * Composes objects into tree-like structures to represent part-whole hierarchies.
+ * Allows clients to treat individual objects and compositions uniformly through a common interface.
+ * Useful for file systems, UI components, and hierarchical structures.
+ *
+ *
+ *
+ * ================================
+ * (Future)
+ * ================================
+ * - Decorator Pattern
+ * - Facade Pattern
+ * - Flyweight Pattern
+ * - Proxy Pattern
+ */
+public class StructuralPatterns {
+    public static void main(String[] args) {
+        System.out.println("Refer comments for Structural Design Patterns guide.");
+    }
+}
---
+++ src/structuralpatterns/adapter/Adapter.java
@@ -0,0 +1,77 @@
+package structuralpatterns.adapter;
+
+
+/*
+
+- Adapter pattern is used to convert the interface of a class into another interface that clients expect.
+- promotes code reusability by allowing existing functionality to be used in new systems
+
+Core Idea:
+- Client expects Interface A
+- You have Class B
+- Adapter sits in between and translates A <-> B
+
+
+Variations of Adapter Pattern
+1. Class Adapter (Inheritance-based)
+- Uses inheritance
+- Adapter extends the adaptee and implements target interface
+- Since Java doesn't support mutilple inheritance, it is less flexible
+
+class Adapter extends Adaptee implements Target {
+    public void request() {
+        specificRequest();
+    }
+}
+
+
+2. Object Adapter (Composition-based)
+- Uses composition, Has-A relationship
+- Adapter contains the adaptee
+- Flexible and Preferred in Java
+
+class Adapter implements Target {
+    private Adaptee adaptee;
+
+    public void request() {
+        adaptee.specificRequest();
+    }
+}
+*/
+
+//Target interface, what client expects
+interface PaymentProcessor {
+    void pay(double amount);
+}
+
+//Adaptee, existing incompatible class
+class LegacyPaymentSystem {
+    public void makePayment(double amount) {
+        System.out.println("Payment done using legacy system: " + amount);
+    }
+}
+
+//Adapter, Composition-based
+class PaymentAdapter implements PaymentProcessor {
+
+    //Composition
+    private LegacyPaymentSystem legacySystem;
+
+    public PaymentAdapter(LegacyPaymentSystem legacySystem) {
+        this.legacySystem = legacySystem;
+    }
+
+    @Override
+    public void pay(double amount) {
+        // adapting method call
+        legacySystem.makePayment(amount);
+    }
+}
+
+//Client code
+class AdapterMain {
+    public static void main(String[] args) {
+        PaymentProcessor processor = new PaymentAdapter(new LegacyPaymentSystem());
+        processor.pay(100);
+    }
+}
