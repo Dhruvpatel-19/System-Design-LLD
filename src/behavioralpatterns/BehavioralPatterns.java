e18ae2ab7685c606e55d2e6ff17ac5af9a24d025
Dhruv Patel <dhruvpatel19201@gmail.com>
2026-05-02 21:25:00 -0400

    Add ChainOfResponsibility and Command Patterns

---
+++ src/behavioralpatterns/BehavioralPatterns.java
@@ -0,0 +1,7 @@
+package behavioralpatterns;
+
+public class BehavioralPatterns {
+    public static void main(String[] args) {
+        System.out.println("Refer comments for Behavioral Design Patterns guide.");
+    }
+}
